5254e77866a0926d01daf344ad69223f1b92f5f7
muhmedsaeed <mohamed.saied.5755@gmail.com>
2026-07-17 17:26:25 +0300

    update generic repo

--- FreeGency.Infrastructure/Persistence/Repositories/GenericRepository.cs
+++ FreeGency.Infrastructure/Persistence/Repositories/GenericRepository.cs
@@ -1,38 +1,49 @@
 ﻿using FreeGency.Domain.Interfaces.Repositories;
+using FreeGency.Infrastructure.Persistence.Context;
+using Microsoft.EntityFrameworkCore;
 
-namespace FreeGency.Infrastructure.Persistence.Repositories
+namespace FreeGency.Infrastructure.Persistence.Repositories;
+
+public class GenericRepository<TEntity> : IGenericRepository<TEntity>
+    where TEntity : class
 {
-    public class GenericRepository<TEntity> : IGenericRepository<TEntity>
-        where TEntity : class
+
+    protected readonly ApplicationDbContext _context;
+    protected readonly DbSet<TEntity> _dbSet;
+
+    public GenericRepository(ApplicationDbContext context)
     {
-        public Task AddAsync(TEntity entity, CancellationToken ct = default)
-        {
-            throw new NotImplementedException();
-        }
+        _context = context;
+        _dbSet = context.Set<TEntity>();
+    }
 
-        public void Delete(TEntity entity)
-        {
-            throw new NotImplementedException();
-        }
+    public Task AddAsync(TEntity entity, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<bool> ExistsAsync(Guid id, CancellationToken ct = default)
-        {
-            throw new NotImplementedException();
-        }
+    public void Delete(TEntity entity)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken ct = default)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<bool> ExistsAsync(Guid id, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
 
-        public Task<TEntity?> GetByIdAsync(Guid id, CancellationToken ct = default)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<IEnumerable<TEntity>> GetAllAsync(CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
 
-        public void Update(TEntity entity)
-        {
-            throw new NotImplementedException();
-        }
+    public Task<TEntity?> GetByIdAsync(Guid id, CancellationToken ct = default)
+    {
+        throw new NotImplementedException();
+    }
+
+    public void Update(TEntity entity)
+    {
+        throw new NotImplementedException();
     }
 }
