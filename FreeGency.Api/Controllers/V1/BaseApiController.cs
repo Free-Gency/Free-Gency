cd133f0950f9d99007da96957d7fe740eb78999e
youssefrramdan <youfahmed353@gmail.com>
2026-07-24 14:53:50 +0300

    Change HandleResult method visibility to protected in BaseApiController

--- FreeGency.Api/Controllers/V1/BaseApiController.cs
+++ FreeGency.Api/Controllers/V1/BaseApiController.cs
@@ -5,7 +5,7 @@ namespace FreeGency.Api.Controllers.V1
     [ApiController]
     public class BaseApiController : ControllerBase
     {
-        public IActionResult HandleResult(ApiResponse result)
+        protected IActionResult HandleResult(ApiResponse result)
         {
             if (result.IsSuccess)
                 return Ok(result);
