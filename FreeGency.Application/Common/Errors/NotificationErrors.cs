a5c3b7a93c5160aebc2c2dae9d0a572256e042df
besar30 <bedobesar19@gmail.com>
2026-08-11 21:06:05 +0300

    add mark as seen endpoint and signalr

--- FreeGency.Application/Common/Errors/NotificationErrors.cs
+++ FreeGency.Application/Common/Errors/NotificationErrors.cs
@@ -8,5 +8,7 @@ namespace FreeGency.Application.Common.Errors
     public static class NotificationErrors
     {
         public static readonly Error SettingNotificationNotFound = new Error("Setting. Setting Notfication Not Found", "Setting Notfication Not Found", StatusCodes.Status404NotFound);
+        public static readonly Error NotificationNotFound = new Error("Notfication. Notfication Not Found", " Notfication Not Found", StatusCodes.Status404NotFound);
+
     }
 }
