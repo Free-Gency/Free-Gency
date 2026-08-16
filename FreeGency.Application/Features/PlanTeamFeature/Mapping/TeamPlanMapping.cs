e2a76f11ba09f45944fad5a18bafa78fb9669a53
muhmedsaeed <mohamed.saied.5755@gmail.com>
2026-08-16 09:18:35 +0300

    fix issues.

--- FreeGency.Application/Features/PlanTeamFeature/Mapping/TeamPlanMapping.cs
+++ FreeGency.Application/Features/PlanTeamFeature/Mapping/TeamPlanMapping.cs
@@ -1,27 +1,24 @@
 ﻿using FreeGency.Application.Features.PlanTeamFeature.Dtos;
 using FreeGency.Domain.Entities.TeamPlans;
-using System;
-using System.Collections.Generic;
-using System.Text;
 
-namespace FreeGency.Application.Features.PlanTeamFeature.Mapping
+
+namespace FreeGency.Application.Features.PlanTeamFeature.Mapping;
+
+public static class TeamPlanMapping
 {
-    public static class TeamPlanMapping
+    public static PlanTeamDto ToDto(this TeamPlan teamPlan)
     {
-        public static PlanTeamDto ToDto(this TeamPlan teamPlan)
-        {
-            return new PlanTeamDto{
-                Id=teamPlan.Id,
-                Name=teamPlan.Name,
-                MonthlyPrice=teamPlan.MonthlyPrice.Value,
-                YearlyPrice=teamPlan.YearlyPrice.Value,
-                Features=teamPlan.Features.Select((x=>new PlanFeatuerTeamDto
-                {
-                    Feature=x.Feature.ToString(),
-                    IsEnabled=x.IsEnabled,
-                    Limit=x.Limit
-                })).ToList()
-            };
-        }
+        return new PlanTeamDto{
+            Id=teamPlan.Id,
+            Name=teamPlan.Name,
+            MonthlyPrice=teamPlan.MonthlyPrice ?? 0,
+            YearlyPrice=teamPlan.YearlyPrice ?? 0,
+            Features=teamPlan.Features.Select((x=>new PlanFeatuerTeamDto
+            {
+                Feature=x.Feature.ToString(),
+                IsEnabled=x.IsEnabled,
+                Limit=x.Limit
+            })).ToList()
+        };
     }
 }
--- FreeGency.Infrastructure/Persistence/Repositories/PlansTeam/TeamPlanRepository.cs
+++ FreeGency.Infrastructure/Persistence/Repositories/PlansTeam/TeamPlanRepository.cs
@@ -1,24 +1,19 @@
 ﻿using FreeGency.Domain.Entities.TeamPlans;
 using FreeGency.Domain.Interfaces.Repositories.plansTeam;
-using Org.BouncyCastle.Asn1;
-using System;
-using System.Collections.Generic;
-using System.Numerics;
-using System.Text;
 
-namespace FreeGency.Infrastructure.Persistence.Repositories.PlansTeam
+
+namespace FreeGency.Infrastructure.Persistence.Repositories.PlansTeam;
+
+public class TeamPlanRepository:GenericRepository<TeamPlan>,ITeamPlanRepository
 {
-    public class TeamPlanRepository:GenericRepository<TeamPlan>,ITeamPlanRepository
+    public TeamPlanRepository(ApplicationDbContext dbContext):base(dbContext)
     {
-        public TeamPlanRepository(ApplicationDbContext dbContext):base(dbContext)
-        {
-            
-        }
-
-        public IQueryable<TeamPlan> GetTeamPlanQueryable()
-        {
-            return _dbSet.AsQueryable();
-        }
-       
+        
     }
+
+    public IQueryable<TeamPlan> GetTeamPlanQueryable()
+    {
+        return _dbSet.AsQueryable().Include(tp => tp.Features).AsQueryable();
+    }
+   
 }
